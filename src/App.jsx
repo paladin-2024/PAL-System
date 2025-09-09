1be1bb00bdae79f04a96e71860cad573a306fac7
YANNICK kabunga <yankbg2@gmail.com>
2025-09-09 23:15:52 +0300

    replace LoginSignUp component with Home component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import LoginSignUp from "./LoginSignup/LoginSignUp.jsx";
+import Home from "./HomePage/Home.jsx";
 
 function App() {
 
   return (
 
       <div>
-            <LoginSignUp/>
+            <Home/>
       </div>
 
   )
